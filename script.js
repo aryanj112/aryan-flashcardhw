6a2372d2d4868c2413f9783e609988430c194b1e
Aryan Jain <2006aryanj@gmail.com>
2024-10-23 16:31:36 -0400

    took html, css, and js from Kimber's github and added logic for buttons

---
+++ script.js
@@ -0,0 +1,50 @@
+const flashcards = [
+    { term: "HTML", definition: "HyperText Markup Language" },
+    { term: "CSS", definition: "Cascading Style Sheets" },
+    { term: "JavaScript", definition: "Programming language of the web" }
+];
+
+// You can use flashcards.length to get the length of the array
+
+// These two variables will come in handy
+let currentIndex = 0;
+let showingTerm = true;
+
+// Start with this function to simply display the card
+function displayCard() {
+    
+    if (showingTerm){
+        document.getElementById("card-content").innerText = flashcards[currentIndex].term
+    }
+    else{
+        document.getElementById("card-content").innerText = flashcards[currentIndex].definition
+    }
+}
+
+// The rest of the code you will write is apart of event listeners
+
+let nextBtn = document.getElementById("next-btn")
+let prevBtn = document.getElementById("prev-btn")
+
+nextBtn.addEventListener("click", () => {
+    if(currentIndex === 2){
+        currentIndex = 0;
+    }
+    else{
+        currentIndex++;
+    }
+})
+
+prevBtn.addEventListener("click", () => {
+    if(currentIndex === 0){
+        currentIndex = 2;
+    }
+    else{
+        currentIndex--;
+    }
+})
+
+
+
+// This line will display the card when the page is refreshed
+window.onload = displayCard;
